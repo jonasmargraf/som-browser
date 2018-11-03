9569313e585b0c570b8fac3fa3ffafba7bad7c3d
Jonas Margraf <jonasmargraf@me.com>
2018-11-03 18:03:33 +0100

    moved MapLabel component to separate file

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,35 +1,11 @@
 import '../assets/css/App.css';
 import MapNode from './MapNode';
 import MapSubnode from './MapSubnode';
+import MapLabel from './MapLabel'
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom'
-
-class MapLabel extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componentDidUpdate() {
-    // console.log('MapLabel updated')
-  }
-
-  render() {
-
-    return (
-      <span
-        className="label"
-        style={
-          {
-            left: this.props.label.labelPosition[0],
-            top: this.props.label.labelPosition[1],
-            display: this.props.label.showLabel ? 'block' : 'none'
-          }
-        }>
-        {this.props.label.showLabel && this.props.label.label}
-      </span>
-    )
-  }
-}
+import { findDOMNode } from 'react-dom';
+import { DragSource } from 'react-dnd';
+import { Types } from './Constants';
 
 class Map extends React.Component {
   constructor(props) {
